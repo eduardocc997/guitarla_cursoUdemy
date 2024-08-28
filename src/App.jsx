28b59f45df58c832222262946cb36eb5a4ffee28
Eduardo Camacho <eduardocc97@hotmail.com>
2024-08-27 19:42:03 -0600

    Me quede por iniciar video 76

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,9 @@ function App() {
 
     return (
         <>
-            <Header></Header>
+            <Header
+                cart = {cart}
+            />
 
             <main className="container-xl mt-5">
                 <h2 className="text-center">Nuestra Colección</h2>
