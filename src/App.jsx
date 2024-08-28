8e6a33ef448668c7a46137e4e3ed04fddaa7367f
Eduardo Camacho <eduardocc97@hotmail.com>
2024-08-28 17:22:53 -0600

    Se termino la seccion, ya funciona el carrito de compras, iniciare la seccion 7, video 85

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
-import { useEffect } from "react"
 import{db} from "./data/db"
 
 function App() {
 
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem('cart');
+        return localStorageCart ? JSON.parse(localStorageCart) : [];
+    }
+
     //State
     const [data, setData] = useState(db)
-    const[cart, setCart] = useState([])
+    const[cart, setCart] = useState(initialCart)
+
+    const MAX_ITEMS = 5
+    const MIN_ITEMS = 1
+
+    useEffect( () => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
 
     function addToCart(item){
 
         const itemExists = cart.findIndex((guitar) => guitar.id == item.id); //Traemos la posicion del elemento en el arreglo si existe, si no, mostrara -1
         if(itemExists >= 0){
             const updatedCart = [...cart] //Creamos una copia de lo que hay en el state para no mutar el original
-            updatedCart[itemExists].quantity++  //Agregamos 1 a la cantidad de ese elemento,   itemExists es la posicion del elemento en el arreglo
-            setCart(updatedCart) //Seteamos el nuevo valor del arreglo con el quantity actualizado
+            if(updatedCart[itemExists].quantity < MAX_ITEMS){ //Evitar agreguen más de la cantidad establecida
+                updatedCart[itemExists].quantity++  //Agregamos 1 a la cantidad de ese elemento,   itemExists es la posicion del elemento en el arreglo
+                setCart(updatedCart) //Seteamos el nuevo valor del arreglo con el quantity actualizado
+            }
         }
         else{
             item.quantity = 1; //Agregamos la propiedad quantity
@@ -24,10 +37,48 @@ function App() {
         }
     }
 
+    function removeFromCart(id){
+        setCart(prevCart => prevCart.filter(guitar => guitar.id != id))
+    }
+
+    function increaseQuantity(id){
+        const updatedCart = cart.map(item => {
+            if(item.id === id && item.quantity < MAX_ITEMS){
+                return{
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function decreaseQuantity(id){
+        const updatedCart = cart.map(item => {
+            if(item.id === id && item.quantity > MIN_ITEMS){
+                return{
+                    ...item,
+                    quantity: item.quantity - 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function clearCart(){
+        setCart([]);
+    }
+
     return (
         <>
             <Header
                 cart = {cart}
+                removeFromCart = {removeFromCart}
+                increaseQuantity = {increaseQuantity}
+                decreaseQuantity = {decreaseQuantity}
+                clearCart = {clearCart}
             />
 
             <main className="container-xl mt-5">
@@ -39,7 +90,7 @@ function App() {
                             key = {guitar.id}
                             guitar = {guitar}
                             setCart = {setCart}
-                            addToCart ={addToCart}
+                            addToCart = {addToCart}
                         />
                     ))}
                 </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
+import { useMemo } from "react"
 
 
-export default function Header({cart}) {
-
+export default function Header({cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart}) {
     
     //State derivado
-    const isEmpty = () => cart.length === 0
-    const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0)
+    const isEmpty = useMemo(() => cart.length === 0, [cart]) //UseMemo solo renderizará el componente cuando el elemento dentro de los corchetes se acctualice, eso mejorará el rendimiento y ahorrara consultas a la API
+    const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
 
     return (
         <header className="py-5 header">
@@ -23,7 +23,7 @@ export default function Header({cart}) {
                             <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
-                                {isEmpty() ? (
+                                {isEmpty ? (
                                     <p className="text-center">El carrito esta vacio</p>
                                 ) : (
                                     <>
@@ -51,6 +51,7 @@ export default function Header({cart}) {
                                                     <button
                                                         type="button"
                                                         className="btn btn-dark"
+                                                        onClick={() => decreaseQuantity(guitar.id)}
                                                     >
                                                         -
                                                     </button>
@@ -58,6 +59,7 @@ export default function Header({cart}) {
                                                     <button
                                                         type="button"
                                                         className="btn btn-dark"
+                                                        onClick={() => increaseQuantity(guitar.id)}
                                                     >
                                                         +
                                                     </button>
@@ -66,6 +68,7 @@ export default function Header({cart}) {
                                                     <button
                                                         className="btn btn-danger"
                                                         type="button"
+                                                        onClick={() => removeFromCart(guitar.id)}
                                                     >
                                                         X
                                                     </button>
@@ -74,8 +77,13 @@ export default function Header({cart}) {
                                         ))}
                                     </tbody>
                                 </table>
-                                <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
-                                <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                                <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
+                                <button 
+                                    className="btn btn-dark w-100 mt-3 p-2"
+                                    onClick={clearCart}
+                                >
+                                    Vaciar Carrito
+                                </button>
                                 </>
                                 )}
                             </div>
